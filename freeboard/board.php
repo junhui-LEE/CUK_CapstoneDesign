cb697b6e3534b6df3c6a564966e219245a09d11a
kkj0419 <72663337+kkj0419@users.noreply.github.com>
2021-04-07 21:34:45 +0900

    Update board.php
    board 테이블 사용
    http://cukdesign21.dothome.co.kr/board.php

--- freeboard/board.php
+++ freeboard/board.php
@@ -1,8 +1,9 @@
 <?php
-#session_start();       #login session
-$list_size=5;           #list_size
+#session_start();
+$list_size=5;
 
 ?>
+
 
 <html>
 <style>
@@ -10,46 +11,45 @@ $list_size=5;           #list_size
 <body>
 
 <?php
-include ('conn.php');  #mysql-connect 
-  
-#printboard
-if(!isset($_GET['idx'])){ 
-        echo "<script>location.href='board.php?idx=1';</script>";
+include ('conn.php');		#mysql-connect
+// global $conn;
+
+if(!isset($_GET['idx'])){
+	echo "<script>location.href='board.php?idx=1';</script>";
 }
 else{
-        $idx=$_GET['idx']; }
+	$idx=$_GET['idx']; }
 
-$idx=($idx-1)*$list_size;    #5
-#$query="select * from { } order by idx desc limit " . $idx . "," . $list_size;
-$query="select * from test order by idx desc limit " . $idx . "," . $list_size;
-$result=mysql_query($query,$conn);
+$idx=($idx-1)*$list_size;
+#
+// $query="select * from test order by idx desc limit " . $idx . "," . $list_size;	//local
+$query="select * from board order by idx desc limit " . $idx . "," . $list_size;
+$result=mysqli_query($GLOBALS['conn'],$query);
+// $result=mysqli_query($query);
+// $result=mysql_query($query);
+$page_cnt=mysqli_num_rows($result);
 
-#table
 echo "<table border='1'>";
-while($row=mysql_fetch_array($result)){
-        echo "<tr>";
-        echo "<td>" . $row['idx'] . "</td>";
-        echo "<td> <a href='board.php?idx=" . ($row['idx']) . "'>" . $row['subject'] . "</a></td>";
-        echo "<td>" . $row['name'] . "</td>";
+while($row=mysqli_fetch_array($result)){
+	echo "<tr>";
+	echo "<td>" . $row['idx'] . "</td>";
+	echo "<td><a href='board.php?idx=" . ($row['idx']) . "'>" . $row['subject'] . "</a></td>";
+        echo "<td>" . $row['user_name'] . "</td>";
         echo "<td>" . date("Y-m-d",strtotime($row['date'])) . "</td>";
         echo "</tr>";
 }
 echo "</table>";
 
-
-$cquery="select * from test";
-#$cquery="select * from { }";
-$cresult=mysql_query($cquery,$conn);
-$page_cnt=mysql_num_rows($cresult);
 $page_cnt=ceil($page_cnt/$list_size);
 
 #paging button
 for($i=0;$i<$page_cnt;$i++){
-        if($i+1==$_GET['idx']){ #selected
-                echo "<input type='button' style='margin:3px;' value='" . ($i+1) . "' onclick=\"location.href='board.php?idx=" . ($i+1) . "'\"disabled>";
-        }
-        else{
-                echo "<input type='button' value='" . ($i+1) . "' style='margin:3px;' onclick=\"location.href='board.php?idx=" . ($i+1) . "'\" >";
-        }
+	if($i+1==$_GET['idx']){
+		echo "<input type='button' style='margin:3px;' value='" . ($i+1) . "'onclick=\"location.href='board.php?idx=" . ($i+1) . "'\" disabled>";
+    }
+
+	else{
+		echo "<input type='button' value='" . ($i+1) . "' style='margin:2px;' onclick=\"location.href='board.php?idx=" . ($i+1) . "'\" >";
+	}
 }
 ?>
